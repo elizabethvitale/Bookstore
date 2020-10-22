2e303e5fd1f1ee6fa37f17124350ecc7e5a21469
Elizabeth Vitale <elizabethavitale@gmail.com>
2020-10-22 14:13:16 -0400

    System sucessfully sends email

--- src/register.java
+++ src/register.java
@@ -24,11 +24,12 @@ import javax.mail.MessagingException;
 import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.PasswordAuthentication;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart; 
-
+import java.util.Properties;
  
 @WebServlet("/register")
 public class register extends HttpServlet {
@@ -60,8 +61,9 @@ public class register extends HttpServlet {
 	User new_user = new User(randomNum, pwd, fname, lname, email, number, "Inactive", false);
 	int register_user = new backendUser().register(new_user);	
 	if(register_user == 0){
-	//send registration email....
+		sendEmail(email,"test");
 	}
+
 
 
 	if(fname=="" || email=="" || number =="" || pwd==""){
@@ -74,22 +76,30 @@ public class register extends HttpServlet {
 	response.sendRedirect("/index.jsp");
 	}
 }
-	public static void sendEmail(Session session,String emailAdd,String body){
+	public static void sendEmail(String emailAdd,String body){
+		String host = "smtp.gmail.com";
+		final String username="ugabookstore2020@gmail.com";
+		final String password="Ugabookstore1!";
+		Properties properties = System.getProperties();
+		properties.put("mail.smtp.host", host);
+		properties.put("mail.smtp.ssl.enable","true");
+		properties.put("mail.smtp.auth","true");
+		Session session = Session.getInstance(properties, new javax.mail.Authenticator(){
+			protected PasswordAuthentication getPasswordAuthentication(){
+			return new PasswordAuthentication(username,password);
+		}});
 		try{
 		MimeMessage msg = new MimeMessage(session);
 	      	msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
-	      	msg.addHeader("format", "flowed");
-	      	msg.addHeader("Content-Transfer-Encoding", "8bit");
-		msg.setFrom(new InternetAddress("ugabookstore@books.com", "NoReply-JD"));
-		msg.setReplyTo(InternetAddress.parse("ugabookstore@books.com",false));
+		msg.setFrom(new InternetAddress("ugabookstore2020@gmail.com"));
 		msg.setSubject("Registration Email Confirmation", "UTF-8");
-		msg.setText(body, "UTF-8");
+		msg.setText("tester", "UTF-8");
 		msg.setSentDate(new Date());
 	      	msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(emailAdd, false));
 	      	System.out.println("Message is ready");
     	  	Transport.send(msg);  
 
-	      	System.out.println("EMail Sent Successfully!!");
+	      	System.out.println("Email Sent Successfully!!");
 		}catch(Exception e){
 			e.printStackTrace();
 		}
