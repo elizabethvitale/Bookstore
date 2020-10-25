c8f9e033326ec19fb69ab3a3e867e708826421f6
Elizabeth Vitale <elizabethavitale@gmail.com>
2020-10-25 13:39:21 -0400

    sends email on sucessful password change

--- src/loggedUpdatePwd.java
+++ src/loggedUpdatePwd.java
@@ -19,6 +19,22 @@ import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import javax.servlet.http.HttpSession;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.PasswordAuthentication;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart; 
+import java.util.Properties;
+ 
  
 
  
@@ -35,7 +51,7 @@ public class loggedUpdatePwd extends HttpServlet {
 		String pwd1 = request.getParameter("newPwd1");
 		String pwd2 = request.getParameter("newPwd2");
 		String userpwd = request.getParameter("currentPwd");
-		
+		String email = (String)session.getAttribute("email");
 		if(!pwd1.equals(pwd2)){
 			response.sendRedirect("/error.jsp"); //doesnt exist...
 			return;
@@ -62,6 +78,11 @@ public class loggedUpdatePwd extends HttpServlet {
 			stmt = con.createStatement(); 
 			query = "update customer set password='" + password + "' where customerid='" + id + "';";
 			int rt = stmt.executeUpdate(query);
+			if(rt == 1){
+				sendEmail(email, "You have successfully reset your password. If you did not request to change your password, please change it immediately.");
+			}
+
+
 		}catch(Exception e){
 			System.out.println(e.getMessage());
 		}
@@ -87,5 +108,34 @@ public class loggedUpdatePwd extends HttpServlet {
                         throw new RuntimeException(e);
                 }
         }
+
+
+	public static void sendEmail(String emailAdd,String body){
+		String host = "smtp.gmail.com";
+		final String username="ugabookstore2020@gmail.com";
+		final String password="Ugabookstore1!";
+		Properties properties = System.getProperties();
+		properties.put("mail.smtp.host", host);
+		properties.put("mail.smtp.ssl.enable","true");
+		properties.put("mail.smtp.auth","true");
+		Session session = Session.getInstance(properties, new javax.mail.Authenticator(){
+			protected PasswordAuthentication getPasswordAuthentication(){
+			return new PasswordAuthentication(username,password);
+		}});
+		try{
+		MimeMessage msg = new MimeMessage(session);
+	      	msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+		msg.setFrom(new InternetAddress("ugabookstore2020@gmail.com"));
+		msg.setSubject("Registration Email Confirmation", "UTF-8");
+		msg.setText(body, "UTF-8");
+	      	msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(emailAdd, false));
+	      	System.out.println("Message is ready");
+    	  	Transport.send(msg);  
+
+	      	System.out.println("Email Sent Successfully!!");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 
 }
