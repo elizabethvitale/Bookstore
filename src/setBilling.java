f559795cc8f56c87853c18a14e94011b7323cdc0
Julia Scott <45429633+jscott1124@users.noreply.github.com>
2020-11-30 19:40:38 -0500

    Simplified checkout

--- src/setBilling.java
+++ src/setBilling.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpSession;
 import javax.servlet.http.Cookie;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 
@@ -22,30 +25,86 @@ public class setBilling extends HttpServlet {
 		HttpSession session = request.getSession();
 
 		String card = request.getParameter("ordercard");
-		String cardinfo = null;
+		String cardinfo = "";
+		String sameShip = request.getParameter("sameshipping");
 
 		if (card.equalsIgnoreCase(String.valueOf(session.getAttribute("cardNumber")))) {
 			cardinfo = String.valueOf(session.getAttribute("expirationDate")) + " " +
 					String.valueOf(session.getAttribute("cardType"));
+
+			session.setAttribute("ordercard", request.getParameter("ordercard"));
+			session.setAttribute("cardinfo", cardinfo);
+			session.setAttribute("billFName", request.getParameter("firstName"));
+			session.setAttribute("billLName", request.getParameter("lastName"));
+			session.setAttribute("billStreet", request.getParameter("street"));
+			session.setAttribute("billCity", request.getParameter("city"));
+			session.setAttribute("billState", request.getParameter("state"));
+			session.setAttribute("billZip", request.getParameter("zipcode"));
+	
+			if (sameShip != null) {
+				session.setAttribute("shipFName", request.getParameter("firstName"));
+				session.setAttribute("shipLName", request.getParameter("lastName"));
+				session.setAttribute("shipStreet", request.getParameter("street"));
+				session.setAttribute("shipCity", request.getParameter("city"));
+				session.setAttribute("shipState", request.getParameter("state"));
+				session.setAttribute("shipZip", request.getParameter("zipcode"));
+				response.sendRedirect("/checkout/checkout-review.jsp");
+			} else {
+				response.sendRedirect("/checkout/checkout-shipping.jsp");
+			}
 		} else if (card.equalsIgnoreCase(String.valueOf(session.getAttribute("cardNumber2")))) {
 			cardinfo = String.valueOf(session.getAttribute("expirationDate2")) + " " +
 					String.valueOf(session.getAttribute("cardType2"));
+
+			session.setAttribute("ordercard", request.getParameter("ordercard"));
+			session.setAttribute("cardinfo", cardinfo);
+			session.setAttribute("billFName", request.getParameter("firstName"));
+			session.setAttribute("billLName", request.getParameter("lastName"));
+			session.setAttribute("billStreet", request.getParameter("street"));
+			session.setAttribute("billCity", request.getParameter("city"));
+			session.setAttribute("billState", request.getParameter("state"));
+			session.setAttribute("billZip", request.getParameter("zipcode"));
+	
+			if (sameShip != null) {
+				session.setAttribute("shipFName", request.getParameter("firstName"));
+				session.setAttribute("shipLName", request.getParameter("lastName"));
+				session.setAttribute("shipStreet", request.getParameter("street"));
+				session.setAttribute("shipCity", request.getParameter("city"));
+				session.setAttribute("shipState", request.getParameter("state"));
+				session.setAttribute("shipZip", request.getParameter("zipcode"));
+				response.sendRedirect("/checkout/checkout-review.jsp");
+			} else {
+				response.sendRedirect("/checkout/checkout-shipping.jsp");
+			}
 		} else if (card.equalsIgnoreCase(String.valueOf(session.getAttribute("cardNumber3")))) {
 			cardinfo = String.valueOf(session.getAttribute("expirationDate3")) + " " +
 					String.valueOf(session.getAttribute("cardType3"));
+
+			session.setAttribute("ordercard", request.getParameter("ordercard"));
+			session.setAttribute("cardinfo", cardinfo);
+			session.setAttribute("billFName", request.getParameter("firstName"));
+			session.setAttribute("billLName", request.getParameter("lastName"));
+			session.setAttribute("billStreet", request.getParameter("street"));
+			session.setAttribute("billCity", request.getParameter("city"));
+			session.setAttribute("billState", request.getParameter("state"));
+			session.setAttribute("billZip", request.getParameter("zipcode"));
+			
+			if (sameShip != null) {
+				session.setAttribute("shipFName", request.getParameter("firstName"));
+				session.setAttribute("shipLName", request.getParameter("lastName"));
+				session.setAttribute("shipStreet", request.getParameter("street"));
+				session.setAttribute("shipCity", request.getParameter("city"));
+				session.setAttribute("shipState", request.getParameter("state"));
+				session.setAttribute("shipZip", request.getParameter("zipcode"));
+				response.sendRedirect("/checkout/checkout-review.jsp");
+			} else {
+				response.sendRedirect("/checkout/checkout-shipping.jsp");
+			}
+
 		} else if (card.equalsIgnoreCase("")) {
 			response.sendRedirect("/checkout/checkout-billing.jsp");
 		}
 
-		session.setAttribute("ordercard", request.getParameter("ordercard"));
-		session.setAttribute("cardinfo", cardinfo);
-		session.setAttribute("billFName", request.getParameter("firstName"));
-		session.setAttribute("billLName", request.getParameter("lastName"));
-		session.setAttribute("billStreet", request.getParameter("street"));
-		session.setAttribute("billCity", request.getParameter("city"));
-		session.setAttribute("billState", request.getParameter("state"));
-		session.setAttribute("billZip", request.getParameter("zipcode"));
-	
-		response.sendRedirect("/checkout/checkout-shipping.jsp");
+
 	}
 }
